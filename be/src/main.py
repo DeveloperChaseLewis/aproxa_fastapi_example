0e2a9919dfbf09f186d230ee68fecb822607dc7c
chase-lewis-akasa <chase.lewis@akasa.com>
2022-03-24 16:47:13 +0000

    fix some issues

--- be/src/main.py
+++ be/src/main.py
@@ -1,8 +1,7 @@
-import uvicorn
 from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
-def root(request: Request):
-    return { "message": "Hello World", "headers": request.headers }
+def root():
+    return { "message": "Hello World" }
